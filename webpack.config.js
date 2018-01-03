8606d3f040d20aa23e5f6a03e75a5f258b8489ec
李靖 <lijing@lijingdeMacBook-Pro.local>
2018-01-03 14:38:23 +0800

    require scss

--- webpack.config.js
+++ webpack.config.js
@@ -69,3 +69,63 @@ module.exports = {
         })
     ]
 };
+
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const path = require('path');
+// const shelljs = require('shelljs');
+// const _ = require('lodash');
+
+// let entry = {};
+// let jsArr = [];
+
+// jsArr.push(path.join(__dirname, `/public/js/*.js`));
+
+// shelljs.ls(jsArr).forEach((val) => {
+//     let name = _.slice(val.split('/'), -1)[0];
+
+//     name = name.split('.')[0];
+//     entry[name] = val;
+// });
+
+// module.exports = {
+//     entry: entry, // 入口文件
+//     output: { // 输出文件
+//         filename: '[name].js', // 输出文件名
+//         path: path.join(__dirname, '/public/build/js') // 输出文件路径
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /.scss$/,
+//                 use: ExtractTextPlugin.extract({
+//                     fallback: 'style-loader',
+//                     use: [
+//                         {
+//                             loader: 'css-loader'
+//                         }, {
+//                             loader: 'sass-loader'
+//                         }
+//                     ]
+//                 })
+//             }
+//         ]
+//     },
+//     plugins: [
+//         new UglifyJsPlugin({
+//             parallel: true,
+//             sourceMap: true,
+//             uglifyOptions: {
+//                 compress: {
+//                     warnings: false
+//                 },
+//                 comments: false
+//             }
+//         }),
+//         new ExtractTextPlugin({
+//             filename: (getPath) => {
+//                 return getPath('../css/[name].css');
+//             }
+//         })
+//     ]
+// };
